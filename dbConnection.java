8217913571f85eee28c80d20683087debec8af4f
sherrylynntower <stower@ca.ibm.com>
2024-03-01 14:29:02 -0400

    Add files via upload

--- dbConnection.java
+++ dbConnection.java
@@ -7,12 +7,30 @@ public class dbConnection {
 	Connection conn = null;
 	
 	public static Connection ConnectDB() {
+		//String jdbcUrl = "jdbc:sqlite://9.42.208.242\\instanceOfMsSQL;databaseName=igniteCensus";
+		//String server = "9.42.208.242";
+		//String port =":3306/";
+	   // String usern ="/exttest2";
+	   // String pass ="strext14";
+	    //IP Address: 9.42.208.242
+	    //User id: /exttest2
+	    //Pass: strext14
+
 		
 		try {
-			Class.forName("org.sqlite.JDBC");
-			Connection conn = DriverManager.getConnection("jdbc:sqlite:C://sqlite/db/igniteCensus.db");
 			
-			//System.out.println("Connection Succeeded");
+			Class.forName("org.sqlite.JDBC");
+			
+			
+			//SQLITE CONNECTION//
+			//Connection conn = DriverManager.getConnection("jdbc:sqlite:C://sqlite/db/igniteCensus.db");
+			
+			//SERVER CONNECTION FOR MYSQL//
+			Connection conn = DriverManager.getConnection("jdbc:mysql://9.42.208.242:3307/ignitecensus","root", "Winter2024");  
+			
+			//MySQL CONNECTION LOCALHOST//
+			//Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ignitecensus","root", "Winter@2024");
+		
 			return conn;
 		}
 		catch (Exception e) {
